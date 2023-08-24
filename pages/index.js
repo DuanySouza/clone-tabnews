8488dc53252187f97498016be507daa249a60720
Duany Souza <duanysouza01@gmail.com>
2023-08-24 11:35:17 +0000

    Nova mensagem na index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Teste</h1>
+    return <h1>Conceda-me a serenidade para aceitar as coisas que não posso mudar, a coragem para mudar as coisas que posso e a sabedoria para saber a diferença.</h1>
 }
 
 export default Home
